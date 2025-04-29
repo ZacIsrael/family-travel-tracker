2b761d215c8f01eb4c54ddb1387b895ac485da7a
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-29 11:16:59 -0400

    Implemented the /user route which displays the countries that the specified user has visited.

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const countries = "countries";
 // name of the table in the postgreSQL database that stores the users
 const usersTable = "users";
 
+// let currentUserId = 1;
 
-let currentUserId = 1;
-
+// variable that will be used to store the users returned from queries
 let users = [
   // { id: 1, name: "Angela", color: "teal" },
   // { id: 2, name: "Jack", color: "powderblue" },
@@ -52,10 +52,10 @@ app.get("/", async (req, res) => {
   try {
     // retrieve the users from the users table in the postgreSQL database
     const allUsers = await db.query(`SELECT * FROM ${usersTable}`);
-    console.log('allUsers.rows = ', allUsers.rows);
+    console.log("allUsers.rows = ", allUsers.rows);
     // assign the users variable to the rows that were returned from the query
     users = allUsers.rows;
-  } catch(err){
+  } catch (err) {
     // an error occured
     console.error("Default GET Route; Error executing query: ", err.stack);
   }
@@ -92,18 +92,82 @@ app.post("/add", async (req, res) => {
   }
 });
 
-// adds a user
+// displays the selected user's respective map
 app.post("/user", async (req, res) => {
-  console.log('req.body = ', req.body);
+  console.log("req.body = ", req.body);
+  // retrieve the id of the user from the request
+  let userId = req.body.user;
 
+  // variable that stores the result from the query
+  let neededUser;
+
+  try {
+    // get the user with id = userId
+    neededUser = await db.query(`SELECT * FROM ${usersTable} WHERE id = ($1)`, [
+      userId,
+    ]);
+    console.log("neededUser = ", neededUser);
+
+    // variable that stores the user and will be sent to the ejs file
+    users = neededUser.rows;
+  } catch (err) {
+    // an error occured
+    console.error(`\'/user\' POST Route; Error executing query that retrives the user with id = ${userId}: `, err.stack);
+  }
+
+  // if we reach this point, needUser is not undefined
+  if (neededUser.rowCount !== 1) {
+    // error; problem with database because more than 1 user has the same id
+    console.error(
+      `Database Error: The ${usersTable} table contains more than 1 user with id = ${userId}`
+    );
+  } else {
+    // found the user with id = userId
+    let retrievedUser = neededUser.rows[0];
+    console.log("retrievedUser = ", retrievedUser);
+
+    // retrieve that user's visited countries
+
+    // variable that stores the result from the query
+    let visitedCountriesByUserQuery;
+
+    try {
+      // retrieve the countries that this user has visited
+      visitedCountriesByUserQuery = await db.query(
+        `SELECT * FROM ${visitedCountriesTable} WHERE user_id = ($1)`,
+        [userId]
+      );
+      console.log("visitedCountriesByUser = ", visitedCountriesByUserQuery);
+    } catch (err) {
+      // an error occured
+      console.error(`\'/user\' POST Route; Error executing query that retrieves the countries for user with id = ${userId}: `, err.stack);
+    }
+
+    // the rows returned from the request
+    let visitedCountriesByUser = visitedCountriesByUserQuery.rows;
+
+    // clean up the data so that only the country codes will be sent to the EJS file (and not the id generated from postgreSQL)
+    let visitedCountries = [];
+    visitedCountriesByUser.forEach((val) => {
+      visitedCountries.push(val.country_code);
+    });
+
+    // send the necessary data to the index ejs file
+    res.render("index.ejs", {
+      countries: visitedCountries,
+      total: visitedCountries.length,
+      users: users,
+      color: retrievedUser.color,
+    });
+
+
+  }
 });
 
 // render new.ejs
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
-
-
 });
 
 app.listen(port, () => {
